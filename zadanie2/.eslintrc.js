576ba28f23d55eb54c38051e61e88ece11c3e97c
agnieszka <agnieszka.siewiera@gmail.com>
2022-03-30 22:49:05 +0200

    first version of chat

---
+++ zadanie2/.eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb', 'prettier'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier'],
+  rules: {
+    'prettier/prettier': [
+      // "prettier/prettier" rule from "eslint-plugin-prettier"
+      'error', // violation of the rule will cause an error
+      {
+        singleQuote: true, // option that sets single quotes as the valid quotes
+      },
+    ],
+    'react/jsx-filename-extension': [
+      // "react/jsx-filename-extension" rule from "eslint-plugin-react"
+      'warn', // violation of the rule will cause a warning
+      {
+        extensions: ['.js', '.jsx'], // option to allow using JSX syntax within both ".js" and ".jsx" file extensions
+      },
+    ],
+  },
+};
---
+++ zadanie2/src/components/Chat/Chat.jsx
@@ -0,0 +1,33 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+import styles from './Chat.module.css';
+
+function Chat({ history }) {
+  if (!Array.isArray(history) || history.length === 0) return null;
+  console.log(history);
+  return (
+    <div className={styles.chat}>
+      <ul>
+        {history.map((singleChat, index) => (
+          // eslint-disable-next-line react/no-array-index-key
+          <li key={index}>
+            <p>
+              <snap className={styles.chatPerson}>{singleChat.person}</snap> :{' '}
+              <snap className={styles.chatMessage}>{singleChat.message}</snap>
+            </p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+// Chat.propTypes = {
+//   history: propTypes.arrayOf(Object),
+// };
+
+// Chat.defaultProps = {
+//   history: [],
+// };
+
+export default Chat;
